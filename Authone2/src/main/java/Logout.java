3709e80b24e634f6cd06d8c6383f1fd10b0089c3
arvindsrini2000 <69394691+arvindsrini2000@users.noreply.github.com>
2022-02-11 20:38:57 +0530

    Update Logout.java

--- Authone2/src/main/java/Logout.java
+++ Authone2/src/main/java/Logout.java
@@ -3,6 +3,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,11 +17,17 @@ public class Logout extends HttpServlet
 	private static final long serialVersionUID = 1L;
        
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
 	{
-		HttpSession session = req.getSession();
-		session.removeAttribute("username");
-		session.invalidate();
+		//HttpSession session = req.getSession();
+		//session.removeAttribute("username");
+		//session.invalidate();
+		Cookie usercookie=new Cookie("username","");  
+        usercookie.setMaxAge(0);
+        //usercookie.setValue(null);
+        res.addCookie(usercookie);  
+        
+
 		showMessageDialog(null, "You have successfully been logged out!");
 		res.sendRedirect("index.jsp");
 	}
